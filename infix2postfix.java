f978d4bdac1f22bd7bd98b881daa5e7408777b2a
shayimsh <48460130+shayimsh@users.noreply.github.com>
2021-04-07 14:11:59 -0400

    Infix to postfix class

---
+++ infix2postfix.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+public class infix2postfix {
+
+    static int precedence(char c){
+        switch (c){
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
