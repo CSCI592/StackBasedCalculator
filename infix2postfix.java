385c3f77d5546ba87d9bf863b5db77e2f115a357
shayimsh <48460130+shayimsh@users.noreply.github.com>
2021-04-07 14:13:03 -0400

    Update infix2postfix.java

--- infix2postfix.java
+++ infix2postfix.java
@@ -15,3 +15,35 @@ public class infix2postfix {
         }
         return -1;
     }
+
+    static String infixToPostFix(String expression){
+
+      String result = "";
+      Stack<Character> stack = new Stack<>();
+      for (int i = 0; i <expression.length() ; i++) {
+          char c = expression.charAt(i);
+
+          //check if char is operator
+          if(precedence(c)>0){
+              while(stack.isEmpty()==false && precedence(stack.peek())>=precedence(c)){
+                  result += stack.pop();
+              }
+              stack.push(c);
+          }else if(c==')'){
+              char x = stack.pop();
+              while(x!='('){
+                  result += x;
+                  x = stack.pop();
+              }
+          }else if(c=='('){
+              stack.push(c);
+          }else{
+              //character is neither operator nor ( 
+              result += c;
+          }
+      }
+      for (int i = 0; i <=stack.size() ; i++) {
+          result += stack.pop();
+      }
+      return result;
+  }
