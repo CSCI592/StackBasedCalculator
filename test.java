043971adb959ac9d18df4279164be1ffa3af65ac
shayimsh <48460130+shayimsh@users.noreply.github.com>
2021-04-10 18:53:07 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,10 +1,10 @@
 public class test{
 
     public static void main(String[] args) {
-        String exp1 = "7+9";
+        String exp1 = "7+9*(2^4-8)*(9+2*1)-6";
         //infix2postfix postExp = new infix2postfix();
         String exp2 = infix2postfix.infixToPostFix(exp1);
-      //System.out.println(exp2);
+      System.out.println(exp2);
       System.out.println(evaluate.evaluatePostfix(exp2));
   
     }
