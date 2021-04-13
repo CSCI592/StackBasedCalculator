8855184f0a000dc911cc6316ce1c767e3c36dda3
shayimsh <48460130+shayimsh@users.noreply.github.com>
2021-04-13 16:33:18 -0400

    testing the code

--- test.java
+++ test.java
@@ -1,11 +1,11 @@
 public class test{
 
     public static void main(String[] args) {
-        String exp1 = "7+99";
+        String exp1 = "5(9*2)";
         //infix2postfix postExp = new infix2postfix();
-        String exp2 = infix2postfix.infixToPostFix(exp1);
+        Object exp2 = evaluate.evaluatePostfix(infix2postfix.infixToPostFix(exp1));
       System.out.println(exp2);
-      System.out.println(evaluate.evaluatePostfix(exp2));
+      //System.out.println(evaluate.evaluatePostfix(exp2));
   
     }
   }
